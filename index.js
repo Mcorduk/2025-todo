b07d78e9d9b31d5cae57944e2cdb163fe6b111fe
Mcorduk <om.corduk@gmail.com>
2025-01-11 22:49:54 +0300

    create tasks, todo list factories

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
 
-console.log(greeting);
+function createTask(description) {
+    return {
+      description: description,
+      completed: false,
+      markCompleted() {
+        this.completed = true;
+      },
+      markIncomplete() {
+        this.completed = false;
+      },
+    };
+  }
+  
+function createTodoList() {
+    const tasks = [];
+  
+    return {
+      addTask(description) {
+        const task = createTask(description);
+        tasks.push(task);
+      },
+      removeTask(index) {
+        tasks.splice(index, 1);
+      },
+      getTasks() {
+        return tasks;
+      },
+    };
+  }
+  
+const todoList = createTodoList();
+
+todoList.addTask('Learn JavaScript');
+todoList.addTask('Build a to-do app');
+todoList.addTask('Write tests');
+
+function displayTasks() {
+  const tasks = todoList.getTasks();
+  tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task.description} - ${task.completed ? 'Completed' : 'Pending'}`);
+  });
+}
+
+todoList.getTasks()[0].markCompleted();
+
+displayTasks();
