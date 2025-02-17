46d5eb016534a50ad8311d63709b3811bdad6784
Mcorduk <om.corduk@gmail.com>
2025-02-17 14:18:19 +0300

    refactor: seperate constants used in multiple places

---
+++ src/script/constants.js
@@ -0,0 +1,20 @@
+  const TASK_PROPERTY = {
+    STATUS: "status",
+    PRIORITY: "priority",
+  }
+  
+  const TASK_STATUS = {
+    INCOMPLETE: "incomplete",
+    INPROGRESS: "inprogress",
+    COMPLETE: "complete",
+  };
+  
+  const TASK_PRIORITY = {
+    LOW: "low",
+    MEDIUM: "medium",
+    HIGH: "high",
+    EXTREME: "extreme",
+  };
+  
+  export { TASK_PROPERTY, TASK_STATUS, TASK_PRIORITY };
+  
