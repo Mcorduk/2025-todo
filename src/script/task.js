3906c28961bf03868c12a4d6f36e9efcee663356
Mcorduk <om.corduk@gmail.com>
2025-02-17 14:19:03 +0300

    refactor: to use new constants file

--- src/script/task.js
+++ src/script/task.js
@@ -1,9 +1,11 @@
+import { TASK_STATUS, TASK_PRIORITY } from "./constants";
+
 class Task {
 
-    static STATUSES = ["incomplete", "inprogress", "complete"];
-    static PRIORITIES = ["low", "medium", "high", "extreme"];
+    static STATUSES = TASK_STATUS;
+    static PRIORITIES = TASK_PRIORITY;
 
-    constructor(name, description, time, date, status = "incomplete", priority = "low") {
+    constructor(name, description, time, date, status = TASK_STATUS.INCOMPLETE, priority = TASK_PRIORITY.LOW) {
       this._name = name;
       this._description = description;
       this._time = time;
@@ -25,6 +27,7 @@ class Task {
     get priority() { return this._priority; }
 
     set status(newStatus) {
+      const STATUSES = Object.values(TASK_STATUS)
       if (!STATUSES.includes(newStatus)) {
         console.log("Please pick an appropriate status.");
         return;
@@ -34,7 +37,8 @@ class Task {
     }
 
     set priority(newPriority) {
-      if (!PRIORITIES.includes(newPriority)) {
+      const TASK_PRIORITIES = Object.values(TASK_PRIORITY);
+      if (!TASK_PRIORITIES.includes(newPriority)) {
         console.log("Please pick an appropriate priority.");
         return;
       }
