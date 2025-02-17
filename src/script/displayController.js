23e1b00e816db24945599bf3a234390096249bd8
Mcorduk <om.corduk@gmail.com>
2025-02-17 16:39:41 +0300

    feat: create displayController and test it for rendering navigation

--- src/script/displayController.js
+++ src/script/displayController.js
@@ -1,12 +1,69 @@
+"use strict";
+
+import { Todo } from './todo';
+import { format } from 'date-fns'
+
 class DisplayController {
     constructor() {
-  
+        this._todoList = [];
+        this._currentTodo = new Todo("Your Things");
     }
-  
-    renderTodo() {
-        const app = document.getElementById('app');
 
-        const nav = document.createElement('nav');
-        // const leftHeader = document.
+    get todoList() { return this._todoList; }
+  
+    get currentTodo() { return this._currentTodo; }
+
+    renderNav(){
+
+        const currentTodo = this.currentTodo;
+
+        const TITLE = currentTodo.name;
+        
+        const ACTIVE_TASK_COUNT = currentTodo.getTaskCount("status", "complete");
+        const COMPLETE_TASK_COUNT = currentTodo.getTaskCount("status", "complete");
+    
+        const today = format(new Date(), 'MMM d, yyyy')
+
+        const navHtml =  `
+            <div id="leftHeader">
+                <div id="sidenav">
+                    <button class="hamb" aria-label="Open Menu"><span class="sr-only">Open Menu</span><svg class="ham" viewBox="0 0 100 100"><path class="line top" d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path class="line middle" d="m 30,50 h 40"></path><path class="line bottom" d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg></button>
+                </div>
+                <div id="todoHeader">
+                    <h1 class="montserrat">${TITLE}</h1>
+                </div>
+                <div id="currentDate" class="open-sans">
+                ${today}
+                </div>
+            </div>
+
+            <div id="rightHeader">
+                <div class="flex-1"></div>
+                <div id="todoTaskStatuses" class="flex-1">
+                    <div id="completedTasks" class="flex-1">
+                        <div class="number montserrat">${ACTIVE_TASK_COUNT}</div>
+                        <div class="open-sans gray">Active</div>
+                    </div>
+                    <div id="incompleteTasks" class="flex-1">
+                        <div class="number montserrat">${COMPLETE_TASK_COUNT}</div>
+                        <div class="open-sans gray">Done</div>
+                    </div>
+                </div>
+                <div id="completionPercentage"class="flex-1">
+                    <p class="gray">65% done</p>
+                </div>
+            </div>
+        `
+
+        let nav = document.querySelector('nav');
+        nav.innerHTML = '';
+
+        nav.innerHTML = navHtml;
+    }
+
+    renderTodo() {
+   
     }
   }
+
+  export { DisplayController };
--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import "../css/common-style.css";
 import "../css/body-style.css";
 import "../css/nav-style.css";
@@ -7,12 +9,31 @@ import "../css/reset.css";
 
 import { Todo } from "./todo";
 import { Task } from "./task";
+import { DisplayController } from "./displayController";
 
-let TODO = new Todo("Test Todo");
+const displayController = new DisplayController()
+
+let todoList = [];
+let todo1 = new Todo("Test Todo 1");
 
 let task1 = new Task("Coding Todo App", "Code Vanilla JS Todo App",null, null, "progressing", "extreme")
 let task2 = new Task("Doing Leetcode", "Study Data Structures and Algorithms", null, null, "progressing", "medium")
 
+let todo2 = new Todo("Test Todo 2");
 
-TODO.push(task1, task2)
-console.log(TODO)
+todo1.addTask(task1);
+todo1.addTask(task2)
+
+todoList.push(todo1)
+
+console.log(todoList)
+let task3 = new Task("Brewing Coffee", "Just kidding im broke",null, null, "progressing", "extreme")
+let task4 = new Task("Doing SQL", "I am too stupid even for SQL...", null, null, "progressing", "medium")
+
+todo2.addTask(task3);
+todo2.addTask(task4)
+
+todoList.push(todo2)
+console.log(todoList)
+
+displayController.renderNav();
