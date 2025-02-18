a0c657596692bd05d6704ad6463ec97e999f6611
Mcorduk <om.corduk@gmail.com>
2025-02-18 12:13:45 +0300

    feat: render footer and completion percentage in nav

--- src/script/displayController.js
+++ src/script/displayController.js
@@ -2,20 +2,24 @@
 
 import { Todo } from './todo';
 import { format } from 'date-fns'
-import { MATERIAL_ICONS } from './constants';
+import { MATERIAL_ICONS, TASK_STATUS } from './constants';
 
 class DisplayController {
     constructor() {
         this._todoList = [];
         this._currentTodo = new Todo("Your Things");
 
-        this.renderNav();
-        this.renderIconSelect();
+        this.renderTodo()
     }
 
     get todoList() { return this._todoList; }
   
     get currentTodo() { return this._currentTodo; }
+
+    set currentTodo(Todo) { 
+        this._currentTodo = Todo
+        this.renderTodo()
+    }
 
     renderNav(){
 
@@ -23,8 +27,10 @@ class DisplayController {
 
         const TITLE = currentTodo.name;
         
-        const ACTIVE_TASK_COUNT = currentTodo.getTaskCount("status", "complete");
-        const COMPLETE_TASK_COUNT = currentTodo.getTaskCount("status", "complete");
+        const ACTIVE_TASK_COUNT = currentTodo.getTaskCount("status", TASK_STATUS.INCOMPLETE);
+        const COMPLETE_TASK_COUNT = currentTodo.getTaskCount("status", TASK_STATUS.COMPLETE);
+        const TOTAL_TASK_COUNT  = ACTIVE_TASK_COUNT + COMPLETE_TASK_COUNT;
+        const COMPLETION_PERCENTAGE = Math.round((COMPLETE_TASK_COUNT/TOTAL_TASK_COUNT)*100)
     
         const today = format(new Date(), 'MMM d, yyyy')
 
@@ -54,7 +60,7 @@ class DisplayController {
                     </div>
                 </div>
                 <div id="completionPercentage"class="flex-1">
-                    <p class="gray">65% done</p>
+                    <p class="gray">${COMPLETION_PERCENTAGE}% done</p>
                 </div>
             </div>
         `
@@ -64,9 +70,62 @@ class DisplayController {
 
         nav.innerHTML = navHtml;
     }
+    renderBody() {
+
+        let taskList = document.getElementById("taskList");
+
+        let taskIndex = 0;
+        for (let task of this.currentTodo.tasks )
+        {
+        const taskHtml = this.renderTask(taskIndex, task)
+        taskList.innerHTML += taskHtml;
+
+        taskIndex++;
+        }
+    }
+
+    renderFooter() {
+        let countSpan = document.querySelector('#completedTaskCount span')
+        const COMPLETE_TASK_COUNT = this.currentTodo.getTaskCount("status", TASK_STATUS.COMPLETE);
+
+        countSpan.textContent = COMPLETE_TASK_COUNT;
+    }
 
     renderTodo() {
-   
+        this.renderNav();
+        this.renderBody();
+        this.renderFooter();
+        this.renderIconSelect();
+    }
+
+    renderTask( index, task ) {
+        console.log("rendering this todo of " + task.name)
+
+        const taskIndex = index;
+        const taskIcon = task.icon
+        const taskDescription = task.description
+        const taskTitle = task.name
+        const taskTime = task.time
+
+        const taskHtml = `
+                <div class="task" data-task-index="${taskIndex}">
+                    <div class="flex">
+                        <div id="taskIcon">
+                            <span class="material-symbols-sharp blue-icon">
+                                ${taskIcon}
+                            </span>
+                        </div>
+                        <div id="taskText">
+                            <div id="taskHeader" class="dark-slate montserrat">${taskTitle}</div>
+                            <div id="taskDescription" class="dark-gray open-sans">${taskDescription}</div>
+                        </div>
+                    </div>
+                    <div id="taskTime" class="dark-gray montserrat">${taskTime}</div>
+                </div>
+                <hr>
+        `
+
+        return taskHtml
     }
 
     renderIconSelect() {
