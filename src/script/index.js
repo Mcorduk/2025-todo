d1a75bf962b2548ba6e7e4f385c36f3456b9f653
Mcorduk <om.corduk@gmail.com>
2025-02-19 13:19:17 +0300

    chore: remove unnecessary imports

--- src/script/index.js
+++ src/script/index.js
@@ -5,19 +5,17 @@ import "../css/body-style.css";
 import "../css/nav-style.css";
 import "../css/add-task-dialog.css";
 import "../css/icon-select-dialog.css";
+import "../css/task-hover.css";
 import "../css/hamburger.css";
 import "../css/reset.css";
 
-import { Todo } from "./todo";
-import { Task } from "./task";
 import { TodoController } from "./todoController";
 import { DisplayController } from "./displayController";
-import { AddTaskController } from "./addTaskController";
-import { TASK_STATUS, TASK_PRIORITY } from "./constants";
+import { TaskController } from "./taskController";
 
 const todoController = new TodoController();
 const displayController = new DisplayController(todoController);
-const addTaskController = new AddTaskController(todoController, displayController);
+const taskController = new TaskController(todoController, displayController);
 
 
 // make hamburger button active
